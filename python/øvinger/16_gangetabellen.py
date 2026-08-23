0266a09b2044106be88495c1d121e3cb7ddded26
Benjamin Skaug <128852215+benjaminskaug@users.noreply.github.com>
2026-08-23 09:00:44 +0200

    legg til sekstende øving i python

---
+++ python/øvinger/16_gangetabellen.py
@@ -0,0 +1,15 @@
+# ============================================================
+# PROSJEKT 16 – GANGETABELLEN
+# ============================================================
+
+print("===== GANGETABELLEN =====")
+
+for i in range(1, 11):
+    
+    print()
+    print(f"--- {i}-gangen ---")
+
+    for j in range(1, 11):
+        svar = i * j
+
+        print(f"{i} x {j} = {svar}")
